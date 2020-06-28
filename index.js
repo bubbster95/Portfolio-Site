dcdd0285355674fbc212ef30272eeef4aba38e1f
bubbster95 <stiles.billy@gmail.com>
2020-06-28 11:11:06 -0400

    Resolved Oversized files jamming push command.

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 accordion = (id, panId) => {
     const button = document.getElementById(id);
     const panel = document.getElementById(panId);
-    if (panel.style.display === "block") {
+    if (panel.style.display === 'flex') {
         button.className = 'tile';
-        panel.style.display = "none";
+        panel.style.display = 'none';
     } else {
         button.className = 'active';
-        panel.style.display = "block";
+        panel.style.display = 'flex';
       }
+}
+
+enlarge = (id, largeId) =>{
+    console.log(id, largeId)
+    const img = document.getElementById(id);
+    // const imgLg = document.getElementById(largeId);
+    if (img.style.display === 'inline-block') {
+        img.style.display = 'none';
+    } else {
+        img.style.display = 'inline-block'
+    }
 }
