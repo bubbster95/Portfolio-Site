d31190ad7a149294ea5f05cc0ec46255d7baa07b
bubbster95 <stiles.billy@gmail.com>
2020-09-01 17:26:14 -0400

    Added filled object with panel content. Added code to switch between slides.

--- index.js
+++ index.js
@@ -17,28 +17,171 @@ const objArr = {
         ]
     ],
     scroll: [
-        ['Scroll Image',
-        'assets/DiscoDanceSprite5.png',
-        'This is a dancing guy again',
-        'gif of guy dancing'
+        ['Infinite Scroll',
+        'assets/Gallery/websites/InfniteScroll.png',
+        'This project is an introduction to React using the "Infinite Scroll" prompt provided by Pramp',
+        'Screen Shot of Infinite Scroll landng page'
         ]
     ],
     turner: [
-        ['Turner Image',
-        'assets/DiscoDanceSprite5.png',
-        'This is a dancing guy again again',
-        'gif of guy dancing'
+        ['Turner\'s Calculator Web Browser',
+        'assets/Gallery/turners-calculator/WebVase.png',
+        'This is the turner\'s calculator. An HTML5 program built to calculate segmented rings for wood workers.',
+        'Screen shot of Turner\'s Calculator web browser layout'
+        ],
+        ['Turner\'s Calculator Mobile Browser',
+        'assets/Gallery/turners-calculator/VaseMobile.png',
+        'The turner\'s calculator was built to work on all devices making it easy to use in the wood shop.',
+        'Screen shot of Turner\'s Calculator mobile browser layout'
+        ],
+        ['Blueprint Table',
+        'assets/Gallery/turners-calculator/WebBluePrints.png',
+        'After submitting your completed design, the Turner\'s calculator will provide a detailed list of all dimensions needed for every ring created. Using these dimensions to cut your lumber creates the trapezoides needed to build your segmented rings.',
+        'Screen shot of blueprints'
+        ],
+        ['Print Preview',
+        'assets/Gallery/turners-calculator/PrintPreview.png',
+        'The last step is to print your blueprints. Blueprints include the table of measurments as well as Trapizoids that can be used to trace your design.',
+        'Screen shot of Trapezoides print preview'
         ]
     ],
     portfolio: [
         ['Portfolio Image',
-        'assets/DiscoDanceSprite5.png',
-        'This is a dancing guy again again',
-        'gif of guy dancing'
+        'assets/Gallery/websites/PortfolioDesign.jpg',
+        'This is a Screen Shot of the old version of the portfolio shite you are on',
+        'Portfolio Site Screen Shot'
+        ]
+    ],
+    motif: [
+        ['Motif Food Issue Cover',
+        'assets/Gallery/Motif/FoodCover.jpg',
+        'Motif Magazine employs Will as a freelance designer and wordpress developer. This Image is cover art done by Will during his time as lead designer at Motif Magazine.',
+        'Motif Food Issue Cover'
+        ],
+        ['Cannabis Issue, Eating Spread',
+        'assets/Gallery/Motif/CannabisEating.jpg',
+        'The Eating section was one of many in the magazine that required special detail and consideration each issue. This issue\'s theme was Cannabis.',
+        'Cannabis Issue Eating Spread'
+        ],
+        ['Tiny Lobster Article',
+        'assets/Gallery/Motif/TinyLobster.jpg',
+        'While Will was the lead designer each issue had a back page devoted to telling a story with images. This issue, Will wrote an article about creating the cover.',
+        'Tiny Lobster back story'
+        ],
+        ['A Taste of Ireland',
+        'assets/Gallery/Motif/Centerspread.jpg',
+        'Will designed a unique centerspread graphic spanning two pages each issue. This centerspread focuses on the beauty of ireland as told by the native irish author and his photography.',
+        'Motif Spring guide centerspread'
+        ],
+        ['A Taste of Antarctica',
+        'assets/Gallery/Motif/AntarcticaSpread.jpg',
+        'This centerspread highlights the beauty of Antarctica. While supporting the story being told by the authors.',
+        'Motif Spring guide centerspread'
+        ],
+        ['Hot Rods Photo Bash',
+        'assets/Gallery/Motif/HotRods.jpg',
+        'Will was often given a folder of photos to create a one page composition to close out each issue of Motif. This one was all hot rods from a local car show.',
+        'Hot Rod Photo Bash'
+        ],
+        ['Whale Guitar Photo Bash',
+        'assets/Gallery/Motif/WhaleGuitar.jpg',
+        'This photo bash tells the story of a local peice of art the comes in the form of a guitar.. and a whale.',
+        'Whale Guitar Photo Bash'
+        ]
+    ],
+    khoo: [
+        ['Khoo Drinks Menu',
+        'assets/Gallery/Khoo/Drinks.jpg',
+        'This is one page of ten pages designed for Khoo Restaurant in order to overhaul their menu and design.',
+        'Khoo Drinks Menu'
+        ],
+        ['Sunday Khoo Deal',
+        'assets/Gallery/Khoo/SundayKhoo.jpg',
+        'This table tent was designed to bring more customers in for sundays.',
+        'Sunday Khoo table tent'
+        ],
+        ['Thirsty Thursday',
+        'assets/Gallery/Khoo/ThirstyThursday.jpg',
+        'Collaborating with the bartenders made for much prittier table tents.',
+        'Thirsty Thursday table tent'
+        ]
+    ],
+    ui: [
+        ['Friend Finder Home Page',
+        'assets/Gallery/UI/FFHomePage.jpg',
+        'Looking to Design a Complete UI, Will created Friend Finder.',
+        'Friend Finder Home Page'
+        ],
+        ['Friend Finder Feed',
+        'assets/Gallery/UI/FFFeed.jpg',
+        'This page is a feed of activities to attend. Friends can post events with little to no notice in hopes of meeting people who are free in the area.',
+        'Friend Finder Feed'
+        ],
+        ['Friend Finder Friends Page',
+        'assets/Gallery/UI/FFFriendsPage.jpg',
+        'Friend finder obviously needs a page to keep track of all the friends you have found using the app.',
+        'Friend Finder Frinds Page'
+        ],
+        ['Friend Finder Flow Chart',
+        'assets/Gallery/UI/FlowChartSmall.jpg',
+        'Friend finder started on flash cards then moved to a basic flow chart before becoming this final document',
+        'Friend Finder flow chart'
+        ],
+        ['Resume Book',
+        'assets/Gallery/UI/ResumeBook.png',
+        'Will created a looser/fun, portfolio website to apply for hasbro. All of the UI elements correspond to the theme of the page they live on.',
+        'Resume Book UI'
+        ]
+    ],
+    comics: [
+        ['Alienation S1 E3',
+        'assets/Gallery/Comics/AlienationS1E3.jpg',
+        'While working as the lead design for Motif Will created a series of in house comics called "Alienation". This comic, like many others reflected the Motif environment.',
+        'Alienation S1 E3'
+        ],
+        ['Alienation S1 E5',
+        'assets/Gallery/Comics/AlienationS1E5FullMin.jpg',
+        'This comic is perticularly notible because it involves two recurring comics in Motif. Will colaborated with the E. (the owner of Nazo Labs) to create this monsterpiece.',
+        'Alienation S1 E5'
+        ],
+        ['Alienation S1 E7',
+        'assets/Gallery/Comics/AlienationS1E7.jpg',
+        'Motif did have a water cooler, but it was never full of booze. Definitely for the better.',
+        'Alienation S1 E7'
+        ]
+    ],
+    animation: [
+        ['The Ultimate Zackatana',
+        '<iframe class="video" width="100%" height="100%"src="https://www.youtube.com/embed/6w8QNKQ0XFU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        'Using Blender Will sculpted and designed a beat saber game asset for his brother. This animation was presented as a christmas present reveal. The saber features trail and lighting effects.',
+        'Zackatana trailer'
+        ],
+        ['Avocado On The Run',
+        '<iframe class="video" width="100%" height="100%"src="https://www.youtube.com/embed/4H05nyf2ph0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        'As a test animation, Will set out to create anything his family asked of him. This Avacado is what was requested and now he is on the lam.',
+        'Avacoda Run Cycle'
+        ],
+        ['2D Portfolio Reel',
+        '<iframe class="video" width="100%" height="100%"src="https://www.youtube.com/embed/Dp-j1I2MjPs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        'This is a reel of 2D animations created by Will.',
+        '2D portfolio Reel'
+        ]
+    ],
+    website: [
+        ['Infinite Scroll',
+        'assets/Gallery/websites/InfniteScroll.png',
+        'This Site is further expleained at <a href="http://127.0.0.1:5500/programming.html">thislink</a>. ',
+        'Infinite Scroll'
+        ],
+        ['Portfolio Site',
+        'assets/Gallery/websites/PortfolioDesign.jpg',
+        'This Site is further expleained at <a href="http://127.0.0.1:5500/programming.html">thislink</a>. ',
+        'Portfolio Site'
         ]
     ]
 };
 
+/* Add content and event listeners to Panels */
 populatePanels = () => {
     let panels = document.getElementsByClassName('panel')
     for (i = 0; i < panels.length; i++) {
@@ -49,63 +192,101 @@ populatePanels = () => {
         closePanel.className = 'closePanel';
         closePanel.innerHTML = 'X';
         thisPanel.appendChild(closePanel)
-        //Slide
+        
         // Previous Slide
         let prev = document.createElement('BUTTON')
         prev.className = 'prev';
         prev.innerHTML = '&#10094';
-        prev.addEventListener('click', previousSlide)
+        prev.addEventListener('click', function() { plusSlide(-1, thisPanel); })
         thisPanel.appendChild(prev)
         //Next Slide
         let next = document.createElement('BUTTON')
         next.className = 'next';
         next.innerHTML = '&#10095';
-        next.addEventListener('click', nextSlide)
+        next.addEventListener('click', function() { plusSlide(1, thisPanel); });
         thisPanel.appendChild(next)
+        // Dot Div
+        let dotDiv = document.createElement('DIV');
+        dotDiv.className = 'dot-div';
+        thisPanel.appendChild(dotDiv);
 
         let thisObjectId = thisPanel.id.slice(0, thisPanel.id.match(/[\\-]/).index);
         let thisObject = objArr[thisObjectId]
+        console.log(thisObject)
         for (j = 0; j < thisObject.length; j++) {
             // Ad Slide
             let slide = document.createElement('DIV');
             slide.className = 'slide-container';
+            slide.id = j;
             thisPanel.appendChild(slide);
             // Add Title
             let title = document.createElement('P');
             title.className = 'image-title';
             title.innerHTML = thisObject[j][0];
             slide.appendChild(title);
-            // Add Images
-            let image = document.createElement('IMG');
-            image.className = 'img';
-            image.alt = thisObject[j][3];
-            image.src = thisObject[j][1];
-            slide.appendChild(image);
+            if (/(?:<\/iframe>)/.test(thisObject[j][1])){
+                // Add video
+                let video = document.createElement('DIV');
+                video.className = 'video';
+                video.alt = thisObject[j][3];
+                video.innerHTML = thisObject[j][1];
+                slide.appendChild(video);  
+            } else {
+                // Add Images
+                let image = document.createElement('IMG');
+                image.className = 'img';
+                image.alt = thisObject[j][3];
+                image.src = thisObject[j][1];
+                slide.appendChild(image);
+            }
+            //Add Dot
+            let dot = document.createElement('SPAN');
+            dot.className = 'dot';
+            console.log(j, "yo")
+            dot.addEventListener('click', function() {currentSlide(parseInt(slide.id)+1, thisPanel); });
+            dotDiv.appendChild(dot);
             //Add Description
             let text = document.createElement('P');
             text.className = 'description';
             text.innerHTML = thisObject[j][2];
             slide.appendChild(text);
-        // console.log('j:', j, thisObject[j])
         }
-
     }
 }
 
-previousSlide = () => {
+/* Panel Controls */
+let slideIndex = 1;
 
+plusSlide = (n, panel) => { showSlides(slideIndex += n, panel); }
+
+currentSlide = (n, panel) => { showSlides(slideIndex = n, panel); }
+
+showSlides = (n, panel) => {
+    // Change Slides
+    let slides = panel.getElementsByClassName("slide-container");
+    if (n > slides.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slides[slideIndex-1].style.display = "grid";
+
+    // Change Dots
+    let dots = panel.getElementsByClassName("dot");
+    for (i = 0; i < dots.length; i++) {
+    console.log(dots[i].id);
+        dots[i].id = '';
+    }
+    dots[slideIndex-1].id += "active";
+    console.log(dots[slideIndex-1].id);
 }
-
-nextSlide = () => {
-
-}
-
-
+/* End Panel Controls */
 
 
 popUp = (e) => {
     let panel = document.getElementById(e.target.id.slice(0, -7) + "-panel");
     panel.style.display = 'block';
+    showSlides(1, panel);
 }
 
 closePopUp = (e) => e.target.parentNode.style.display = 'none';
