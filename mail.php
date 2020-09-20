14726994cd3bc383d49303954239da3ddb8f7fa1
bubbster95 <stiles.billy@gmail.com>
2020-09-20 13:37:49 -0400

    PHP Test

--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "wstiles1@uri.edu";
+$recipient = "wstiles1@my.uri.edu";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
