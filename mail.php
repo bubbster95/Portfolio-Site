c4b0de1342fb0fcfe06355d5a599c60efa29ef3f
bubbster95 <stiles.billy@gmail.com>
2020-09-20 02:22:30 -0400

    PHP test

--- mail.php
+++ mail.php
@@ -1,11 +1,12 @@
-<?php $name = $_POST['name'];
+<?php
+header("Location: index.html");
+$name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
 $recipient = "stiles.billy@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
-header("Location: index.html");
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
 ?>
